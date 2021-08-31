1dcb89a7d7cd0df853f971333265f3f1e946adb3
Vikas Jain <87235644+vikasjain2003@users.noreply.github.com>
2021-08-31 11:38:52 +0530

    Create Arithemetic.java

---
+++ Arithemetic.java
@@ -0,0 +1,17 @@
+class Arthemetic{
+    public static void main(String atr[]) {
+       int a = 10;
+       int b = 0;
+       switch (1) {
+           case 1:
+               System.out.println("Addition result of a and b is: " + (a+b));
+               case 2:
+               System.out.println("Subtraction result of a and b is: " + (a-b));
+               case 3:
+               System.out.println("Multiplication result of a and b is: " + (a*b));
+               case 4:
+               System.out.println("Division resultof a and b is: " + (a/b));
+       }
+
+    }
+}
