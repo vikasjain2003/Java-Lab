3ee072f4a735635f092cd8058a2ea20d13734d33
Vikas Jain <87235644+vikasjain2003@users.noreply.github.com>
2021-08-31 11:10:52 +0530

    Create simpleAddition.java

---
+++ simpleAddition.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String args[]) {
+	    int a,b;
+	    a = 2;
+	    b = 5;
+		System.out.println("the first value " + a);
+		System.out.println("the second value " + b);
+		for (int i=a; i<=b ; i++ ){
+		    System.out.println("the cube of the values are " + i*i*i);
+		} 
+
+	}
+}
